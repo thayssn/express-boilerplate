a01270072996555beb9cd57d615882f9c30fa757
Thays <thays.sn@hotmail.com>
2019-06-29 00:12:48 -0300

    Fix on eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,6 @@ module.exports = {
     "class-methods-use-this" : "off",
     "no-param-reassign": "off",
     "camelcase" : "off",
-    "no-unusd-var": ["error", {"argsIgnorePattern": "next"}]
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
   },
 };
